391a2e48b8076d2f188f99c4c63e1914d7004764
huntpato <huntpatito@gmail.com>
2023-05-11 18:54:15 -0300

    landing creation

--- digital-money-house/src/components/layouts/LayoutStyle.ts
+++ digital-money-house/src/components/layouts/LayoutStyle.ts
@@ -3,13 +3,15 @@ import styled from 'styled-components';
 export const LayoutContainer = styled.div`
     display: flex;
     flex-direction: column;
-    min-height: 100vh;
-    width: 100%;
-    margin: auto;
+    /* min-height: 100vh; */
+    height:100vh;
+    width: 100vw;
+    /* margin: auto; */
 `
 
 export const SectionContainer = styled.div`
-    width: 100vw;
+    width: 100%;
+    height:100%;
     display: flex;
     flex: 1;
     flex-direction: column;
