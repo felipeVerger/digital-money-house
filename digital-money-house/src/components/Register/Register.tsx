dbdeaa2c72bb3e41b7d161fe4f11953379bfb667
gianlucadonofrio <gian.donofrio2000@gmail.com>
2023-05-15 19:05:25 -0300

    Add Mail Verification

--- digital-money-house/src/components/Register/Register.tsx
+++ digital-money-house/src/components/Register/Register.tsx
@@ -1,50 +1,91 @@
-import React from 'react'
-import { RegisterContainer, RegisterBody, Title, Form, FormBlock, SubmitButton } from './RegisterStyle'
-import { useFormContext } from 'react-hook-form'
-import Input from './Input/Input';
+import React from "react";
+import {
+  RegisterContainer,
+  RegisterBody,
+  Title,
+  Form,
+  FormBlock,
+  SubmitButton,
+} from "./RegisterStyle";
+import { useFormContext } from "react-hook-form";
+import Input from "./Input/Input";
+import { log } from "console";
 
 const Register = () => {
-  const { handleSubmit, formState: { errors } } = useFormContext();
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = useFormContext();
 
-  const onSubmit = async (data: any) => {
-    console.log(data);
-  }
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const code = Math.floor(Math.random() * (999999 - 100000)) + 100000;
+    const resp = await fetch("/api/sendVerificationCode", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: e.currentTarget.email.value,
+        code,
+      }),
+    });
+
+    if (resp.status === 200) {
+      const data = await resp.json();
+      localStorage.setItem("code", code.toString());
+      localStorage.setItem("isVerified", "false");
+      console.log(data);
+    }
+  };
 
   return (
     <RegisterContainer>
       <RegisterBody>
         <Title>Crear cuenta</Title>
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form onSubmit={handleFormSubmit}>
           <FormBlock>
             <Input
               name="firstname"
               id="firstname"
               type="text"
               placeholder="Nombre*"
-              errorText={String(errors.firstname?.message === undefined ? "" : errors.firstname?.message)}
+              errorText={String(
+                errors.firstname?.message === undefined
+                  ? ""
+                  : errors.firstname?.message
+              )}
             />
             <Input
               name="lastname"
               id="lastname"
               type="text"
               placeholder="Apellido*"
-              errorText={String(errors.lastname?.message === undefined ? "" : errors.lastname?.message)}
+              errorText={String(
+                errors.lastname?.message === undefined
+                  ? ""
+                  : errors.lastname?.message
+              )}
             />
           </FormBlock>
           <FormBlock>
-            <Input 
-                name="dni" 
-                id="dni" 
-                type="number" 
-                placeholder="DNI*" 
-                errorText={String(errors.dni?.message === undefined ? "" : errors.dni?.message)}
+            <Input
+              name="dni"
+              id="dni"
+              type="number"
+              placeholder="DNI*"
+              errorText={String(
+                errors.dni?.message === undefined ? "" : errors.dni?.message
+              )}
             />
             <Input
               name="email"
               id="email"
               type="email"
               placeholder="Correo electronico*"
-              errorText={String(errors.email?.message === undefined ? "" : errors.email?.message)}
+              errorText={String(
+                errors.email?.message === undefined ? "" : errors.email?.message
+              )}
             />
           </FormBlock>
           <FormBlock>
@@ -53,14 +94,22 @@ const Register = () => {
               id="password"
               type="password"
               placeholder="Contraseña*"
-              errorText={String(errors.password?.message === undefined ? "" : errors.password?.message)}
+              errorText={String(
+                errors.password?.message === undefined
+                  ? ""
+                  : errors.password?.message
+              )}
             />
             <Input
               name="confirmPassword"
               id="confirmPassword"
               type="password"
               placeholder="Confirmar contraseña*"
-              errorText={String(errors.confirmPassword?.message === undefined ? "" : errors.confirmPassword?.message)}
+              errorText={String(
+                errors.confirmPassword?.message === undefined
+                  ? ""
+                  : errors.confirmPassword?.message
+              )}
             />
           </FormBlock>
           <FormBlock>
@@ -69,14 +118,16 @@ const Register = () => {
               id="phone"
               type="text"
               placeholder="Telefono*"
-              errorText={String(errors.phone?.message === undefined ? "" : errors.phone?.message)}
+              errorText={String(
+                errors.phone?.message === undefined ? "" : errors.phone?.message
+              )}
             />
             <SubmitButton type="submit">Crear cuenta</SubmitButton>
           </FormBlock>
         </Form>
       </RegisterBody>
     </RegisterContainer>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
---
+++ digital-money-house/src/pages/api/sendVerificationCode.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import sendgrid from "@sendgrid/mail";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY!);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    await sendgrid.send({
+      to: `${req.body.email}`,
+      from: "registro-dmh-equipo9@gmail.com",
+      subject: "Confimación de registro - Digital Money House",
+      html: `
+        <div>
+          <h1>Verify your email</h1>
+          <p>Your verification code is: ${req.body.code}</p>
+        </div>
+      `,
+      // mailSettings: {
+      //   sandboxMode: {
+      //     enable: process.env.NODE_ENV === "development",
+      //   },
+      // },
+    });
+    res.status(200).json({ message: "Email sent" });
+  } catch (error: any) {
+    console.error(error);
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: "" });
+}
