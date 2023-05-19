3e46803775c87c3b4d70d06a1bf146985080cb40
Santiago Alvarez Eraso <dg.santiago.alvarez@gmail.com>
2023-05-19 10:23:35 -0500

    camibo email archivo send-email.ts

--- digital-money-house/src/pages/api/send-mail.ts
+++ digital-money-house/src/pages/api/send-mail.ts
@@ -11,7 +11,7 @@ export default async function handler(
   try {
     await sendgrid.send({
       to: req.body.email,
-      from: "registro.dmh.equipo9@gmail.com",
+      from: "registrodmh.equipo09@gmail.com",
       subject: "Confimación de registro - Digital Money House",
       html: `<body>
       <center class="wrapper" data-link-color="#1188E6" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#000000; background-color:#FFFFFF;">
