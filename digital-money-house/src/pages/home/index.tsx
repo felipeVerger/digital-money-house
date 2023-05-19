77a3f4a423564e3f066c5c5c5942bcaa3dbf4855
gianlucadonofrio <gian.donofrio2000@gmail.com>
2023-05-18 22:09:01 -0300

    Adding features

--- digital-money-house/src/pages/home/index.tsx
+++ digital-money-house/src/pages/home/index.tsx
@@ -1,8 +1,15 @@
+import Head from "next/head";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>
+        <h1>Home</h1>
+      </div>
+    </>
   );
 };
 
