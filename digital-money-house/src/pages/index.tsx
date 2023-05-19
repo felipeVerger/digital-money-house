cc4127ad1776576f5bd70aa67d44b7ea028bbd55
Santiago Alvarez Eraso <dg.santiago.alvarez@gmail.com>
2023-05-19 09:39:29 -0500

    cambios index

--- digital-money-house/src/pages/index.tsx
+++ digital-money-house/src/pages/index.tsx
@@ -1,12 +1,22 @@
-import Head from 'next/head';
-import { MainContainer, ServicesContainer, CardTitle, GreenBackground, LineTitle, Subtitle, Title, ImgTabletDesktop, ImgMobile } from './indexStyled';
-import CardHome from '@/components/CardHome/CardHome';
-import { data } from '../assets/dataLanding';
-import useMediaQuery from '@/hooks/useMediaQuery';
-import { useEffect } from 'react';
+import Head from "next/head";
+import {
+  MainContainer,
+  ServicesContainer,
+  CardTitle,
+  GreenBackground,
+  LineTitle,
+  Subtitle,
+  Title,
+  ImgTabletDesktop,
+  ImgMobile,
+} from "./indexStyled";
+import CardHome from "@/components/CardHome/CardHome";
+import { data } from "../assets/dataLanding";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
 
 const Home = () => {
-  const isMobile = useMediaQuery('(max-width: 768px)');
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
   return (
     <>
@@ -14,14 +24,29 @@ const Home = () => {
         <title>Digital Money House</title>
         <meta name="description" content="Digital Money House" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MainContainer>
         {isMobile ? (
-          <ImgMobile
-            src="/landingMobile.png"
-            alt="image background" 
-          />
+          <ImgMobile src="/landingMobile.png" alt="image background" />
         ) : (
           <ImgTabletDesktop
             src="/landingDesktopTablet.png"
